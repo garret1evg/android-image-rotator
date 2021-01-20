d76d2e862218cfb02cf38926fabdc8fdade587ea
Evgenii Chmutov <garret.evg@gmail.com>
2021-01-20 16:18:23 +0200

    rotate img

--- app/src/main/java/com/example/image_rotator/MainActivity.java
+++ app/src/main/java/com/example/image_rotator/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.image_rotator;
 
+import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rotatedImg.findViewById(R.id.img_rotate);
+
+        rotatedImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RotateAnimation rotateAnimation = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                        RotateAnimation.RELATIVE_TO_SELF,0.5f);
+                rotateAnimation.setDuration(1000);
+                rotatedImg.startAnimation(rotateAnimation);
+            }
+        });
     }
 }
