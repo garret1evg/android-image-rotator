2eb7022c7fc3dba6bd30c53a24356f6812f74f07
Evgenii Chmutov <garret.evg@gmail.com>
2021-01-20 17:56:37 +0200

    simple slider

--- app/src/main/java/com/example/image_rotator/MainActivity.java
+++ app/src/main/java/com/example/image_rotator/MainActivity.java
@@ -1,30 +1,64 @@
 package com.example.image_rotator;
 
-import android.view.View;
+import android.graphics.Matrix;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView rotatedImg;
+    SeekBar slider;
+    TextView textView;
+    int angle = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rotatedImg.findViewById(R.id.img_rotate);
+        rotatedImg = findViewById(R.id.img_rotate);
+        slider = findViewById(R.id.slider);
+        textView = findViewById(R.id.textView);
 
-        rotatedImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RotateAnimation rotateAnimation = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-                        RotateAnimation.RELATIVE_TO_SELF,0.5f);
-                rotateAnimation.setDuration(1000);
-                rotatedImg.startAnimation(rotateAnimation);
-            }
-        });
+        slider.setOnSeekBarChangeListener(seekBarChangeListener);
+
+//        rotatedImg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                RotateAnimation rotateAnimation = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+//                        RotateAnimation.RELATIVE_TO_SELF,0.5f);
+//                rotateAnimation.setDuration(1000);
+//                rotatedImg.startAnimation(rotateAnimation);
+//            }
+//        });
     }
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            angle = seekBar.getProgress() - 180;
+            final String text = angle + "\u00b0";
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(text);
+                    rotatedImg.setRotation(angle);
+                }
+            });
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
